2f3fa850a69bb28dc9643339c33d4a87312757e7
FredyArevaloHernandez <86616267+FredyArevaloHernandez@users.noreply.github.com>
2021-09-24 23:57:46 -0500

    Add files via upload

--- js/admon.js
+++ js/admon.js
@@ -1,1 +1,16 @@
+let validaUsr = () => {
+
+    let enviarBtn = document.getElementById("enviarBtn");
+    
+
+    enviarBtn.addEventListener('click', (event) => {
+        
+        event.preventDefault();
+        
+                
+
+    })
+}
+
+validaUsr();
 
