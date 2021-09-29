571b53c5b477f919ac7c512427c62a2aded10adf
FredyArevaloHernandez <86616267+FredyArevaloHernandez@users.noreply.github.com>
2021-09-29 11:48:39 -0500

    Add files via upload

--- js/admon.js
+++ js/admon.js
@@ -1,16 +1,51 @@
 let validaUsr = () => {
 
-    let enviarBtn = document.getElementById("enviarBtn");
-    
-
+    let enviarBtn = document.getElementById("enviarBtn");    
     enviarBtn.addEventListener('click', (event) => {
         
         event.preventDefault();
-        
-                
+        let enlacePagina = document.createElement('a');
+        enlacePagina.href = 'adm-intro.html';
+        enlacePagina.click();
 
     })
 }
-
 validaUsr();
+
+// pills-home-tab // Perfiles
+
+// pills-profile-tab // Usuarios
+
+let seleccionTab = () => {
+
+    let tabPerfiles = document.getElementById('pills-home-tab');
+    let tabUsuarios = document.getElementById('pills-profile-tab');
+    let pagePerfiles = document.getElementById('pills-home');
+    let pageUsuarios = document.getElementById('pills-profile')
+
+    tabPerfiles.addEventListener('click', (event) =>{
+
+        // event.preventDefault();
+        tabPerfiles.className='nav-link active';
+        tabUsuarios.className='nav-link';
+        pagePerfiles.className='tab-pane fade show active';
+        pageUsuarios.className='tab-pane fade'        
+
+    })
+
+    tabUsuarios.addEventListener('click', (event) =>{
+
+        // event.preventDefault();
+        tabUsuarios.className='nav-link active';
+        tabPerfiles.className='nav-link';
+        pageUsuarios.className='tab-pane fade show active';
+        pagePerfiles.className='tab-pane fade'
+
+
+    })
+
+}
+seleccionTab();
+
+
 
